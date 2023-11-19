853eaba44d2a9ab1fe03e2e6361fbe4ad13467c9
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-11-19 23:01:58 +0100

    Create Classemobile.py

---
+++ Classemobile.py
@@ -0,0 +1,20 @@
+class Mobile:
+    
+    def __init__(self,CompanyName,storage,serialNum,name,dualSim,support4k):
+        self.CompanyName = CompanyName
+        self.storage = storage
+        self.serialNum = serialNum
+        self.name = name
+        self.dualSim = dualSim
+        self.sipport4k = support4k
+      
+    def call():
+        pass
+        
+    def sendMessage():
+        pass
+    
+    def playMedia():
+        pass
+    
+    
